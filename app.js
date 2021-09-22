d0077486357ce46037a005cd9310bee8e088846e
Your Name <you@example.com>
2021-09-22 11:05:38 +0300

    fonts

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-document.getElementById('input-text-one').addEventListener('keypress', function(event) {
-    document.querySelector('#result-form h1').innerText =  this.value+ event.key;
-})
-document.getElementById('input-text-two').addEventListener('keypress', function(event) {
-  document.querySelector('#result-form h2').innerText = this.value + event.key;
-})
-document.getElementById('input-text-three').addEventListener('keypress', function(event) {
-  document.querySelector('#result-form h3').innerText = this.value + event.key;
-})
+document
+  .getElementById("input-text-one")
+  .addEventListener("keypress", function (event) {
+    document.querySelector("#result-form h1").innerText =
+      this.value + event.key;
+  });
+document
+  .getElementById("input-text-two")
+  .addEventListener("keypress", function (event) {
+    document.querySelector("#result-form h2").innerText =
+      this.value + event.key;
+  });
+document
+  .getElementById("input-text-three")
+  .addEventListener("keypress", function (event) {
+    document.querySelector("#result-form h3").innerText =
+      this.value + event.key;
+  });
