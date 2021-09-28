700ea095162d527ea310c5aabfc05081ad08ef55
Your Name <you@example.com>
2021-09-28 10:01:14 +0300

    zxc

--- app.js
+++ app.js
@@ -16,3 +16,32 @@ document
     document.querySelector("#result-form h3").innerText =
       this.value + event.key;
   });
+
+const inform = document.getElementById("form-inform");
+const save = document.getElementById("btn-save");
+const htmlFile = document.querySelector("html").innerHTML;
+const resForm = document.getElementById("result-form").innerHTML;
+const el = document.querySelector("html").innerHTML;
+var resultat = new XMLSerializer().serializeToString(document)
+
+save.addEventListener("click", () => {
+  if (confirm("Do you want to save the file?")) {
+    console.log(resultat);
+    inform.classList.add("none");
+    fetch(
+      "data:text/html;charset=utf-8," + encodeURI("\uFEFF" + resultat)
+    )
+      .then((res) => res.blob())
+      .then((res) => {
+        const data = window.URL.createObjectURL(res);
+        var link = document.createElement("a");
+        link.href = data;
+        link.download = "busines-card.html";
+        link.click();
+        setTimeout(function () {
+          window.URL.revokeObjectURL(data);
+        }, 100);
+        link.remove();
+      });
+  } else return;
+});
